8ff65c8a3629e69057cc7b20e30e8ba34767a8a3
gibson <kyansimbigibson@gmail.com>
2019-06-17 19:58:44 +0300

    week 2 homework

---
+++ index.js
@@ -0,0 +1,154 @@
+"jsindex.js"
+//1. Write a console.log statement saying "Hello World!" for each language that you know.
+alert("good morning/eng");
+alert("gunayden/turkish");
+alert("ensula/luganda");
+alert("kalimera/greek");
+alert("good morgon/Norwgian");
+console.log(alert);
+
+// The SyntaxError was due to the sigle colon
+console.log("I am awesome");
+x= ("the value of any later ")
+let mynum = x
+console.log(x);
+console.log(mynum);
+//3.3 Add a console.log statement that logs the value of x.
+//3.4 Now initialize your variable x with an integer.
+//3.5 Next, add a console.log statement that explains what you think the value of x is.
+//3.6 Add a console.log statement that logs the value of x
+x =5
+console.log(x);
+let myy =("boys to men");
+console.log(myy);
+myy =("development levels");
+
+console.log(myy);
+//4. Declare a variable y and assign a string to it.
+//4.1 Write a console.log statement in which you explain in words what you think the value of the string is.
+//4.2 Now console.log the variable y.
+//4.3 Now assign a new string to the variable y
+let y="gibson"
+gibson=("it is my name");
+console.log(y + gibson);
+
+// rounding off
+//5. How do you round the number 7.25, to the nearest integer (i.e., whole number)?
+//5.1 Declare a variable z and assign the number 7.25 to it.
+//5.2 Console.log z.
+//5.3 Declare another variable a that has the value of z but rounded to the nearest integer.
+//5.4 Console.log a. 
+let z = Math.round(7.25);
+console.log(z);
+
+
+a =8
+console.log(a);
+
+console.log(a-z);
+
+//a==z
+//a===z
+//a<=z
+//>=z
+console.log(a)
+ z = 7.25
+
+//  n =Number .localecompare(a);
+
+//6.4 Create an array that has your favorite animals inside
+// (see if you can find a good name that exactly describes what this variable will hold).
+
+ let myarray = [ "girafe","cow","sheep","buffalo","lion"];
+ console.log(myarray);
+ 
+//myarray;
+//6.6 Add a statement that adds Daan's favorite animal ('baby pig') to the existing array.
+console.log(myarray.unshift("Daan favorite animal (baby pig)", "gibson"));
+console.log(myarray);
+//7.1 Let's consider the following string: let myString = "this is a test".
+//7.2 Add the string to your file and console.log it.
+//7.3 Find a way to get the length of myString.
+//7.4 Console.log the length of myString.
+
+
+let myString = "this is a test"
+console.log(myString);
+console.log(myString.length);
+
+
+let goat = 100
+    cow = 89
+    console.log(goat>cow);
+    //8.1 First declare at least four variables and assign them different data types. 
+    let w = "women";
+    let age = 30;
+    let data = { "lona is so beautiful" : "she is my girlfreind"}
+    let you;
+// For each variable write a console.log statement that logs the value
+    console.log(w );
+    console.log(age);
+    console.log(data);
+    console.log(you);
+    //9. If x equals 7, and the only other statement is x = x % 3, what would be the new value of x?
+    x=7;
+    x=x%3;
+    console.log(x);
+   // 9.1 Add at least 3 console.log statements in which you show that you understand what % does.  
+   q=500;
+   q=q%13;
+   console.log(q);
+   h=87;
+   h=h%23;
+   console.log(h);
+   t=700;
+   t=t%3;
+   console.log(t);
+   //10. Write a program to answer the following questions:
+//10.1 Can you store multiple types in an array? Numbers and strings?
+let array =["gibbs is a black boy","tall",30];
+console.log(array);
+// 10.2 Can you compare infinities? (Not in Eyad's world) - does 6/0 === 10/0? How can you test this?
+let p = 6/0;
+let e = 10/0;
+//10.3 Add console.log statements to the above program's in which you show that you understand the concepts (just like you've done in the above assignments).
+console.log(p===e)
+
+
+
+
+// let x=5;
+//     (x===5)
+// let myarray = [ "girafe","cow","sheep","buffalo","lion",1,x,];
+// console.log(myarray);
+
+// for ( i=0; i<myarray.length;i++)
+// console.log(i);
+// console.log(myarray[2] ); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
